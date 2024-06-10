dc41e1bda9a0aa5f4ef394c8bddd5c2285cfaa39
malobr <marcelotc1202@gmail.com>
2024-06-10 15:25:53 -0300

    Base do projeto adicionada

---
+++ Chapeu-Seletor/ChapeuSeletor/src/Interfaces/Casas.java
@@ -0,0 +1,10 @@
+package Interfaces;
+
+public interface Casas {
+
+    public String Sonserina();
+    public String Grifinoria();
+    public String Corvinal();
+    public String LufaLufa();
+
+}
